d386f11f6f6ea86eecfbccd3ca627d10b679d27a
dmcirfan <danil.milshin.07.12@gmail.com>
2020-09-16 19:29:39 +0300

    Initial commit

---
+++ src/ru/mirea/labs3/Circle.java
@@ -0,0 +1,46 @@
+package ru.mirea.labs3;
+
+public class Circle extends Shape {
+
+    protected double radius;
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.filled = false;
+        this.color = "blue";
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+    public double getArea() {
+        return 0;
+    }
+
+
+    public double getPerimeter() {
+        return 0;
+    }
+
+
+    public String toString() {
+        return "Circle: " +
+                "radius = " + radius;
+    }
+}
---
+++ src/ru/mirea/labs3/Movable.java
@@ -0,0 +1,8 @@
+package ru.mirea.labs3;
+
+public interface Movable {
+    public void moveUp();
+    public void moveRight();
+    public void moveLeft();
+    public void moveDown();
+}
